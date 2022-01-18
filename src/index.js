03fa6f8acbcd87cdd6a38aa564e2d049df3985d9
Ethan Isola <isolaethan@gmail.com>
2022-01-18 15:35:07 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.body.querySelector("form")
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildToDo(e.target.new_todo.value)
+  form.reset()  
+  }
+  )
+
+
+}
+);
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let del = document.createElement('button')
+  del.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+  del.textContent = 'x'
+  p.textContent = `${todo}` 
+  p.appendChild(del)
+  console.log(p)
+  document.body.querySelector('#list').appendChild(p)
+
+}
+
